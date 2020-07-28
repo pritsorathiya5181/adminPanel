09d57b31b46189036938cec1dccb0729465273c9
prit <prit.sorathiya@gmail.com>
2020-07-28 17:16:36 +0530

    Create Relationships

--- accounts/models.py
+++ accounts/models.py
@@ -11,14 +11,25 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     STATUS = (("Indoor", "Indoor"), ("Outdoor", "Outdoor"))
 
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=STATUS)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -28,7 +39,7 @@ class Order(models.Model):
         ("Delivered", "Delivered"),
     )
 
-    # customer=
-    # product=
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
