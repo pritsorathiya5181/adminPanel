2426b58636b4f7b2967fa946c43ce99d6281f393
prit <prit.sorathiya@gmail.com>
2020-07-28 13:37:03 +0530

    adding templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    # return HttpResponse('Home')
+    return render(request, 'accounts/dashboard.html')
+
+def products(request):
+    return render(request, 'accounts/products.html')
+
+def customer(request):
+    # return HttpResponse('Customer')
+    return render(request, 'accounts/customer.html')
